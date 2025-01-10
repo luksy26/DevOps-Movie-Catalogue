6856d2f37b4f11d03f42628350866597e2e4db63
Lucas Lazaroiu <73948966+luksy26@users.noreply.github.com>
2025-01-11 01:15:25 +0200

    Revert that

--- restartCluster.ps1
+++ restartCluster.ps1
@@ -9,7 +9,7 @@ kubectl apply -f ./KubernetesConfigs/06-api-pod.yaml
 kubectl apply -f ./KubernetesConfigs/07-api-NodePort.yaml
 
 # make sure auth-pod starts only when the other two pods are up
-Start-Sleep -Seconds 10
+Start-Sleep -Seconds 25
 kubectl apply -f ./KubernetesConfigs/08-auth-pod.yaml 
 kubectl apply -f ./KubernetesConfigs/09-auth-ClusterIP.yaml
 
